edc4c7d454e626727fc4403e2107ac637ad1ba4e
Tahir Jadoon <tjadoon@gmail.com>
2021-12-22 02:33:25 -0500

    fireBase AngularFire Restructuring Training

---
+++ prj014FitnessTracker-fireBase-AngularFire-Restructuring-Training/src/app/Site/training/new-training/new-training.component.ts
@@ -0,0 +1,103 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { NgForm } from '@angular/forms';
+import { Subscription } from 'rxjs';
+
+import { ExerciseModel } from '../../../Models/exercise.model';
+
+import { TrainingService } from '../../../Services/training.service';
+
+@Component({
+  selector: 'app-new-training',
+  templateUrl: './new-training.component.html',
+  styleUrls: ['./new-training.component.css']
+})
+export class NewTrainingComponent implements OnInit, OnDestroy {
+
+  exercises: ExerciseModel[] = [];
+  //exercises: Observable<any>;
+  //exercises: Observable<ExerciseModel[]>;
+
+  //subscribe to exercisesFetchedSubject event being thrown by TrainingService
+  exercisesFetchedSubscription: Subscription;
+
+  constructor(private trainingService: TrainingService) { }
+
+  ngOnInit(): void {
+    //initial static data load
+    //this.exercises = this.trainingService.getAvailableExercises();
+
+    //get data from "availableExercises"
+    /*
+    this.dbFireStore.collection('availableExercises').valueChanges().subscribe(result => {
+      console.log(result);
+    });
+    */
+    //get the collection name from the environment file
+    //console.log(environment.fireBaseCollectionNames.availableExercises);
+    /*
+    this.dbFireStore
+        .collection(environment.fireBaseCollectionNames.availableExercises)
+        .snapshotChanges()
+        .subscribe(result => {
+          for(const r of result){
+            console.log(r.payload.doc.id, r.payload.doc.data());
+          }
+        });
+    */
+    /*
+    this.dbFireStore
+        .collection(environment.fireBaseCollectionNames.availableExercises)
+        .snapshotChanges()
+        .pipe(
+          map(docArray => { //map rxjs operator
+            return docArray.map(doc => { //using javascript map to map array to ExerciseModel
+              return {
+                id: doc.payload.doc.id, 
+                ...doc.payload.doc.data() as ExerciseModel
+              };
+            });
+          })
+        )
+        .subscribe(result => {
+          console.log(result);
+        });
+    */
+
+    //assign to exercises 
+    /*
+    this.exercises = this.dbFireStore
+    .collection(environment.fireBaseCollectionNames.availableExercises)
+    .snapshotChanges()
+    .pipe(
+      map(docArray => { //map rxjs operator
+        return docArray.map(doc => { //using javascript map to map array to ExerciseModel
+          return {
+            id: doc.payload.doc.id, 
+            ...doc.payload.doc.data() as ExerciseModel
+          };
+        });
+      })
+    );
+    */
+
+    //subscribe to exercisesFetchedSubject
+    this.exercisesFetchedSubscription = this.trainingService.exercisesFetchedSubject.subscribe(exercises => {
+      console.log("New trainings fetched", exercises);
+      this.exercises = exercises;
+    });
+    //get the data 
+    this.trainingService.getAvailableExercises();
+    
+  }
+
+  ngOnDestroy(): void {
+      if(this.exercisesFetchedSubscription) this.exercisesFetchedSubscription.unsubscribe();
+  }
+
+  onStartTraining(form: NgForm){
+    console.log(form.value.exercise);
+    this.trainingService.startExercise(form.value.exercise);
+  }
+
+}
